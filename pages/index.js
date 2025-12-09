550c43bc895d174ebfee9db1a22b9fa8ab29a450
milhioli <enzo.avelino@alamobeneficios.org.br>
2025-12-09 08:21:57 -0300

    to modify the phrase: add minha noiva

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Minha linda, minha vida, se você me ama, da uma risadinha ;)</h1>
+    return <h1>Minha linda, minha vida, minha noiva, minha Luisa se você me ama, da uma risadinha ;)</h1>
 }
 
 export default Home;
